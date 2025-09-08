3bc23343ef27959bdcc4b29c64006a6a882cc149
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-09-08 09:25:34 +0700

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className="bg-white py-20">
+      <h1>Ini Hero Section</h1>
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                &copy This is Footer Testing
+            </footer>
+        </div>
+    )
+};
